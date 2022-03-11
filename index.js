73c9fb16dde6d49575a4caf0be44418cd2d339c8
aleksa198978 <97627613+aleksa198978@users.noreply.github.com>
2022-03-11 13:42:25 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+pub struct AccountInfo<'a> {
+    pub key: &'a Pubkey,
+    pub is_signer: bool,
+    pub is_writable: bool,
+    pub lamports: Rc<RefCell<&'a mut u64>>,
+    pub data: Rc<RefCell<&'a mut [u8]>>,
+    pub owner: &'a Pubkey,
+    pub executable: bool,
+    pub rent_epoch: u64,
+}
